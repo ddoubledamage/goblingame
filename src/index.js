612c797fd51e66eb0bea54f94c8bc6af2812d7ce
ddoubledamage <47157358+ddoubledamage@users.noreply.github.com>
2024-11-23 20:28:51 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-import './css/style.css';
-import goblinImage from './img/goblin.png';
+import "./css/style.css";
+import goblinImage from "./img/goblin.png";
 
 const gridSize = 4;
-const grid = document.createElement('div');
-grid.className = 'grid';
+const grid = document.createElement("div");
+grid.className = "grid";
 document.body.appendChild(grid);
 
 // Создаем элемент character
-const character = document.createElement('div');
-character.className = 'character';
+const character = document.createElement("div");
+character.className = "character";
 grid.appendChild(character);
 
-for (let i = 0; i < gridSize * gridSize; i++){
-    const cell = document.createElement('div');
-    cell.className = 'cell';
-    grid.appendChild(cell);
+for (let i = 0; i < gridSize * gridSize; i++) {
+  const cell = document.createElement("div");
+  cell.className = "cell";
+  grid.appendChild(cell);
 }
 
-const goblin = document.createElement('img');
+const goblin = document.createElement("img");
 goblin.src = goblinImage;
-goblin.className = 'goblin';
+goblin.className = "goblin";
 grid.appendChild(goblin);
 
-function moveGoblin(){
-    const cells = document.querySelectorAll('.cell');
-    const randomIndex = Math.floor(Math.random() * cells.length);
-    const randomCell = cells[randomIndex];
+function moveGoblin() {
+  const cells = document.querySelectorAll(".cell");
+  const randomIndex = Math.floor(Math.random() * cells.length);
+  const randomCell = cells[randomIndex];
 
-    // Проверяем, находится ли goblin в текущей ячейке
-    if (goblin.parentElement !== randomCell) {
-        randomCell.appendChild(goblin);
-    }
+  // Проверяем, находится ли goblin в текущей ячейке
+  if (goblin.parentElement !== randomCell) {
+    randomCell.appendChild(goblin);
+  }
 }
 
 setInterval(moveGoblin, 1000);
